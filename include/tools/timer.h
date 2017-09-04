0cba74e3aaa3bd5c8fd5b3d1782f35efbd81f1d5
Dai, Pengcheng <13151098@buaa.edu.cn>
2017-09-04 16:37:55 +0800

    Add TestSpeed class for test task multiple times.

--- include/tools/timer.h
+++ include/tools/timer.h
@@ -6,17 +6,56 @@
 #include <string>
 #include <ratio>
 #include <chrono>
+#include <functional>
 
 namespace FeTools {
+    class TestSpeed {
+    public:
+        
+    public:
+        TestSpeed(const std::function<int()> func, unsigned test_num = 100,
+            const std::string description = std::string())
+            : _task_id(++_task_num), _task_description(description), _func(func), _test_num(test_num) {}
+
+        ~TestSpeed() {}
+
+        unsigned set_num(unsigned test_num) {
+            _test_num = test_num;
+            return _test_num;
+        }
+        unsigned get_num() const {
+            return _test_num;
+        }
+
+        void test() const {
+            int sum = 0;
+            auto _begin = std::chrono::system_clock::now();
+            for (int i = 0;i < _test_num;++i)
+                sum += _func();
+            auto _end = std::chrono::system_clock::now();
+            auto _dur = std::chrono::duration_cast<std::chrono::nanoseconds>(_end - _begin);
+            std::cout << "Test speed task " << _task_id << ":\n";
+            if (!_task_description.empty())
+                std::cout << _task_description << '\n';
+            std::cout << "Result sum: " << sum << '\n';
+            std::cout << "Run " << _test_num << " times." << '\n';
+            std::cout << "Mean running time: " << 
+                static_cast<double>(_dur.count()) / _test_num << "ns" <<std::endl;
+        }
+
+    private:
+        static int _task_num;
+        const int _task_id;
+        const std::string _task_description;
+        const std::function<int()> _func;
+        unsigned _test_num;
+    }; //class TestSpeed
+
     class RunningTime {
     public:
         
     public:
-        RunningTime(): _task_id(++_task_num) {
-            _begin_timer();
-        }
-
-        RunningTime(const std::string description)
+        RunningTime(const std::string description = std::string())
             : _task_id(++_task_num), _task_description(description) {
             _begin_timer();
         }
@@ -41,9 +80,10 @@ namespace FeTools {
         const int _task_id;
         const std::string _task_description;
         std::chrono::system_clock::time_point _begin;
-    }; //class RunningTime
+    }; // class RunningTime
     
     inline int RunningTime::_task_num = 0;
+    inline int TestSpeed::_task_num = 0;
 } // namespace FeTools
 
 #endif // FeNAL_TOOLS_TIMER_
