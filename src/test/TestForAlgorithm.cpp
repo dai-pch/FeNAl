1ffe21195ec4ef1e2627d04197a53e979109e6dc
Dai, Pengcheng <13151098@buaa.edu.cn>
2017-09-04 16:39:44 +0800

    Add time test for FFT.

--- src/test/TestForAlgorithm.cpp
+++ src/test/TestForAlgorithm.cpp
@@ -5,9 +5,11 @@
 #include <algorithm>
 #include <cmath>
 #include "number/algrm.h"
+#include "tools/timer.h"
 
 using namespace std;
 using namespace Number;
+using namespace FeTools;
 
 TEST_CASE("Test FFT", "[FFT][Algorithm]") {
     int length = 1024;
@@ -28,4 +30,37 @@ TEST_CASE("Test FFT", "[FFT][Algorithm]") {
         REQUIRE(src[ii] == Approx(ifft[ii]));
     }
 
+}
+
+TEST_CASE("Test FFT profile", "[FFT][Algorithm][profile]") {
+    int length = 1024;
+    double omega = 3.14159265358979 / 8;
+	vector<double> src(length);
+    vector<float> src_f(length);
+
+    for (int ii=0;ii<length;++ii) {
+        src[ii] = cos(omega * ii);
+        src_f[ii] = cos(omega * ii);
+    }
+
+    TestSpeed fft_d(
+        [&](){auto res = FFT(src); return res[0].real();},
+        1000, "1024 point double FFT");
+    fft_d.test();
+    TestSpeed fft_f(
+        [&](){auto res = FFT(src_f); return res[0].real();},
+        1000, "1024 point double FFT");
+    fft_f.test();
+
+    // {
+    //     RunningTime timer("1024 point double FFT");
+    //     auto fft = FFT(src);
+    // }
+    // {
+    //     RunningTime timer("1024 point float FFT");
+    //     auto fft = FFT(src_f);
+    // }
+    {
+        RunningTime timer("Blank baseline.");
+    }
 }
